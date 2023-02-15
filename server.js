1b07aa89ff28dfdf9b4cf5918a58f8e9e09b1864
Elizabeth Czarny <liz.cz91@gmail.com>
2023-02-15 14:45:09 +0100

    path update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('../backend/middleware/errorMiddleware');
+const { errorHandler } = require('../backend/middleware/errorMiddleware.js');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
